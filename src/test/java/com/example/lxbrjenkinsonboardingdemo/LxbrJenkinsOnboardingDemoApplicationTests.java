7198cb5357e192a105c9731f77d551f501f36d09
Tiago Ceccon <ceccon.t@hotmail.com>
2020-01-22 00:13:27 -0300

    Init repo

---
+++ src/test/java/com/example/lxbrjenkinsonboardingdemo/LxbrJenkinsOnboardingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lxbrjenkinsonboardingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxbrJenkinsOnboardingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
